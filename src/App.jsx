dd0dc8fef25b3ace27596b3d6c2ebec6940bfba2
lucas-da-silva <lucassouza00012345@gmail.com>
2022-08-26 16:21:09 -0300

    req-2 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import './App.css';
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <section>
       <Switch>
         <Route exact path="/" render={ (props) => <Login { ...props } /> } />
+        <Route exact path="/carteira" component={ Wallet } />
       </Switch>
     </section>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,28 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <section>
+        <p data-testid="email-field">{email}</p>
+        <div className="expenses-header">
+          <p data-testid="total-field">Despesa Total: R$ 0,00</p>
+          <p data-testid="header-currency-field">BRL</p>
+        </div>
+      </section>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Header);
