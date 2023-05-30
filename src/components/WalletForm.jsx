20b827e803a20f988891d153d4344db6d46a84eb
Lucas da Silva <lucassouza00012345@gmail.com>
2023-05-30 11:44:38 -0300

    feat: create isToEditExpense function and style for WalletForm

--- src/components/WalletForm.jsx
+++ src/components/WalletForm.jsx
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { addExpense, fineshEditForm } from '../redux/actions';
+import { addExpense, fineshEditForm, fineshSetupToEdit } from '../redux/actions';
 import getApi from '../services';
+import '../styles/WalletForm.css';
 
 class WalletForm extends Component {
   state = {
@@ -11,6 +12,16 @@ class WalletForm extends Component {
     currency: 'USD',
     method: 'Dinheiro',
     tag: 'Alimentação',
+  };
+
+  isToEditExpense = () => {
+    const { expenses, idToEdit, dispatch } = this.props;
+    const expenseToEdit = expenses[idToEdit];
+    const { value, description, currency, method, tag } = expenseToEdit;
+    this.setState({
+      value, description, currency, method, tag,
+    });
+    dispatch(fineshSetupToEdit());
   };
 
   handleChange = (event) => {
@@ -39,77 +50,92 @@ class WalletForm extends Component {
   };
 
   render() {
-    const { currencies, editor } = this.props;
+    const { currencies, editor, setupToEdit } = this.props;
     const { value, description, currency, method, tag } = this.state;
+    if (setupToEdit) {
+      this.isToEditExpense();
+    }
     return (
-      <form onSubmit={ (event) => this.submitForm(event, editor) }>
-        <label htmlFor="value-input">
-          Valor:
-          <input
-            id="value-input"
-            type="number"
-            name="value"
-            data-testid="value-input"
-            value={ value }
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label htmlFor="currency-input">
-          <select
-            data-testid="currency-input"
-            name="currency"
-            id="currency-input"
-            value={ currency }
-            onChange={ this.handleChange }
+      <section className="wallet-form-container">
+        <form
+          className="wallet-form"
+          onSubmit={ (event) => this.submitForm(event, editor) }
+        >
+          <div className="input-group value-input">
+            <span className="input-group-text">Valor</span>
+            <input
+              type="number"
+              name="value"
+              data-testid="value-input"
+              className="form-control value-input"
+              aria-label="Amount (to the nearest dollar)"
+              value={ value }
+              onChange={ this.handleChange }
+            />
+          </div>
+          <div className="input-group currency-input">
+            <span className="input-group-text">Moeda</span>
+            <select
+              data-testid="currency-input"
+              name="currency"
+              className="form-select"
+              value={ currency }
+              onChange={ this.handleChange }
+            >
+              {
+                currencies.map((coin) => <option key={ coin }>{coin}</option>)
+              }
+            </select>
+          </div>
+          <div className="input-group method-input">
+            <span className="input-group-text">Método de pagamento</span>
+            <select
+              data-testid="method-input"
+              name="method"
+              className="form-select"
+              value={ method }
+              onChange={ this.handleChange }
+            >
+              <option value="Dinheiro">Dinheiro</option>
+              <option value="Cartão de crédito">Cartão de crédito</option>
+              <option value="Cartão de débito">Cartão de débito</option>
+            </select>
+          </div>
+          <div className="input-group tag-input">
+            <span className="input-group-text">Tag</span>
+            <select
+              data-testid="tag-input"
+              name="tag"
+              className="form-select"
+              value={ tag }
+              onChange={ this.handleChange }
+            >
+              <option value="Alimentação">Alimentação</option>
+              <option value="Lazer">Lazer</option>
+              <option value="Trabalho">Trabalho</option>
+              <option value="Transporte">Transporte</option>
+              <option value="Saúde">Saúde</option>
+            </select>
+          </div>
+          <div className="input-group description-input">
+            <span className="input-group-text">Descrição</span>
+            <input
+              type="text"
+              className="form-control"
+              name="description"
+              data-testid="description-input"
+              value={ description }
+              onChange={ this.handleChange }
+            />
+          </div>
+          <button
+            className={ editor ? 'btn btn-warning' : 'btn btn-primary' }
+            type="submit"
           >
-            {
-              currencies.map((coin) => <option key={ coin }>{coin}</option>)
-            }
-          </select>
-        </label>
-        <label htmlFor="method-input">
-          <select
-            data-testid="method-input"
-            name="method"
-            id="method-input"
-            value={ method }
-            onChange={ this.handleChange }
-          >
-            <option value="Dinheiro">Dinheiro</option>
-            <option value="Cartão de crédito">Cartão de crédito</option>
-            <option value="Cartão de débito">Cartão de débito</option>
-          </select>
-        </label>
-        <label htmlFor="tag-input">
-          <select
-            data-testid="tag-input"
-            name="tag"
-            id="tag-input"
-            value={ tag }
-            onChange={ this.handleChange }
-          >
-            <option value="Alimentação">Alimentação</option>
-            <option value="Lazer">Lazer</option>
-            <option value="Trabalho">Trabalho</option>
-            <option value="Transporte">Transporte</option>
-            <option value="Saúde">Saúde</option>
-          </select>
-        </label>
-        <label htmlFor="description-input">
-          Descrição
-          <input
-            id="description-input"
-            type="text"
-            name="description"
-            data-testid="description-input"
-            value={ description }
-            onChange={ this.handleChange }
-          />
-        </label>
-        <button type="submit">
-          {editor ? 'Editar despesa' : 'Adicionar despesa'}
-        </button>
-      </form>
+            {editor ? 'Editar despesa' : 'Adicionar despesa'}
+          </button>
+        </form>
+      </section>
     );
   }
 }
@@ -120,6 +146,7 @@ const mapStateToProps = ({ wallet }) => ({
   expenses: wallet.expenses,
   editor: wallet.editor,
   idToEdit: wallet.idToEdit,
+  setupToEdit: wallet.setupToEdit,
 });
 
 WalletForm.propTypes = {
@@ -128,6 +155,8 @@ WalletForm.propTypes = {
   dispatch: PropTypes.func.isRequired,
   editor: PropTypes.bool.isRequired,
   idToEdit: PropTypes.number.isRequired,
+  expenses: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  setupToEdit: PropTypes.bool.isRequired,
 };
 
 export default connect(mapStateToProps)(WalletForm);
