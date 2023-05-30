f6ac60d10bc6c50539004b94b6292acbb705c0a7
Lucas da Silva <lucassouza00012345@gmail.com>
2023-05-30 11:41:26 -0300

    style: for Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,28 @@
+import { faSackDollar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import '../styles/Header.css';
 
 class Header extends Component {
   render() {
     const { email, total } = this.props;
     return (
-      <section>
-        <p data-testid="email-field">{email}</p>
-        <div className="expenses-header">
-          <p>Despesa Total: R$</p>
-          <p data-testid="total-field">
-            {total.toFixed(2)}
-          </p>
-          <p data-testid="header-currency-field">BRL</p>
+      <section className="header-container">
+        <div className="title-header">
+          <h3>TrybeWallet</h3>
+          <FontAwesomeIcon className="icon-sack-dollar" icon={ faSackDollar } />
+        </div>
+        <div className="email-total-header">
+          <p className="email-header" data-testid="email-field">{email}</p>
+          <div className="expenses-header">
+            <p>Despesa Total: R$</p>
+            <p data-testid="total-field">
+              {total.toFixed(2)}
+            </p>
+            <p data-testid="header-currency-field">BRL</p>
+          </div>
         </div>
       </section>
     );
